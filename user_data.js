28a5eb7b42eb77553f7099ffc7e10b9bc85b21dd
Lorem1377 <129212991+tibla@users.noreply.github.com>
2025-10-19 09:02:37 +0300

    Добавлен пользователь 1

--- user_data.js
+++ user_data.js
@@ -10,3 +10,16 @@ switch(password_nickname , email_user){
   default:
     alert("у вас что-то не так")
 }
+
+let password_use = "1"
+let email_user = "1"
+switch(password_nickname , email_user){
+  case password:
+    alert("Добро пожаловать\n Ваш пароль " + password_use)
+    break
+  case email_user:
+    alert("Ваша почта " + email_user)
+    break
+  default:
+    alert("у вас что-то не так")
+}
